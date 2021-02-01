acfdc568db3be5dec18f6b0cd964e51f18a60f44
Abhishek Humagain <abhishekhmgn@gmail.com>
2021-02-01 23:47:05 +0545

    Personal Project

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+    // User.findById(1)
+    //   .then(user => {
+    //     req.user = user;
+    //     next();
+    //   })
+    //   .catch(err => console.log(err));
+    next();
 });
 
 app.use('/admin', adminRoutes);
@@ -33,5 +33,5 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(3000);
+    app.listen(process.env.PORT || 3000);
 });
